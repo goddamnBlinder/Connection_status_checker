a6dcc5fe7104fbb04d4db89e95886030db895586
James Chinweike <chinweikechukwudi@gmail.com>
2023-08-18 04:49:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 const connected =document.querySelector(".status-online");
 const disconnected =document.querySelector(".status-offline");
+
+async function connectionStatus(){
+ try {
+   const fetchResult = await fetch("https://cdn.pixabay.com/photo/2023/04/04/04/50/sunset-7898466_1280.jpg"
+    + (new Date ()).getTime());
+    return fetchResult.status >= 200 && fetchResult.status < 300
+ } catch (error) {
+  console.log("connection is down");
+  disconnected.textContent = disconnected.style.display = "block";
+
+ }
+
+}
+
+setInterval(async() => {
+   const result = await connectionStatus();
+   if (result) {
+     connected.textContent = connected.style.display = "block"
+   }
+}, 5000); 
+
+
+window.addEventListener("load", () => {
+  
+})
