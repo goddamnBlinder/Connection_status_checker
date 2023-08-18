2a3628f3dd145bfa910d144c48f26b948112205c
James Chinweike <chinweikechukwudi@gmail.com>
2023-08-18 04:14:19 +0100

    commit continues

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
-
-window.addEventListener('load', (event) => {
-    const statusState = document.querySelector(".status");
-    statusState.textContent = navigator.online ? "onliine" : "online"
-});
-
-window.addEventListener("online", (event) => {
-   statusState.textContent = "Online" ;
-});
-
-window.addEventListener("offline", (event)=>{
-  statusState.textContent = "Offline"
-})
-
-async function statusDisplay (){
-  statusState.textContent 
-}
-
-
+const connected =document.querySelector(".status-online");
+const disconnected =document.querySelector(".status-offline");
