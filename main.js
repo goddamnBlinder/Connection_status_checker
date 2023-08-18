aaa48ef56830f6e6c55b0f4663d1b895d0c7d611
James Chinweike <chinweikechukwudi@gmail.com>
2023-08-18 05:33:56 +0100

    daddy

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-const connected =document.querySelector(".status-online");
-const disconnected =document.querySelector(".status-offline");
+const connected = document.querySelector(".status-online");
+const disconnected = document.querySelector(".status-offline");
+const Pending = document.querySelector(".status-pending");
+
+
 
 async function connectionStatus(){
- try {
-   const fetchResult = await fetch("https://cdn.pixabay.com/photo/2023/04/04/04/50/sunset-7898466_1280.jpg"
-    + (new Date ()).getTime());
-    return fetchResult.status >= 200 && fetchResult.status < 300
- } catch (error) {
-  console.log("connection is down");
-  disconnected.textContent = disconnected.style.display = "block";
-
- }
-
+    try {
+      const fetchResult = await fetch("https://cdn.pixabay.com/photo/2023/04/04/04/50/sunset-7898466_1280.jpg"
+      + (new Date ()).getTime());
+      return fetchResult.status >= 200 && fetchResult.status < 300
+    } catch (error) {
+      console.log("connection is down");
+      disconnected.textContent = disconnected.style.display = 'block';
+      Pending.textContent = Pending.style.display = 'none';
+      
+  }
+  
 }
 
+
+//Observe the connction
 setInterval(async() => {
-   const result = await connectionStatus();
-   if (result) {
-     connected.textContent = connected.style.display = "block"
-   }
+  const result = await connectionStatus();
+  if (result) {
+    connected.textContent = connected.style.display = 'block';
+    Pending.textContent = Pending.style.display = 'none';
+  }
 }, 5000); 
 
 
-window.addEventListener("load", () => {
-  
+//CHecks the connection when the page Loads
+window.addEventListener("load", async (event) => {
+  if(connectionStatus()) {
+    connected.textcontent = connected.style.display = 'block';
+    Pending.textContent = Pending.style.display = 'none';
+  } else{
+    disconnected.textContent = disconnected.style.display = 'block';
+    Pending.textContent = Pending.style.display = 'none';
+    
+  }
 })
